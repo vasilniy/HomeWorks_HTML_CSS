027949344b9ccde14084c85d542fdaefb89c03f8
vasiliy <vasilniy@rambler.ru>
2023-02-15 23:11:18 +0600

    html итоговое

--- Seminar_9/HomeWorks9/script.js
+++ Seminar_9/HomeWorks9/script.js
@@ -3,4 +3,8 @@ const dropMenuEl = document.querySelector('.drop_menu');
 
 navDropMenuEl.addEventListener('click', e => {
     dropMenuEl.classList.toggle('menu_block');   
-})
+});
+
+dropMenuEl.addEventListener('mouseleave', e =>{
+    dropMenuEl.classList.add('menu_block');
+});
